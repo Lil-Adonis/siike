b83fd4031e23aac0fcc28432aae15976c29dde23
Lil-Adonis <adenuaib24@gmail.com>
2022-09-25 08:01:26 -0700

    hero

--- sliike/src/components/Hero.jsx
+++ sliike/src/components/Hero.jsx
@@ -3,11 +3,19 @@ import Diamond from '../assets/hero-image.png';
 
 const Hero = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <img src={Diamond} className="bg-cover bg-center" alt='img' />
+ <div className="hero min-h-screen bg-base-200">
+    <div className="w-full h-full bg-cover bg-center flex-col lg:flex-row-reverse"><img src={Diamond} className="" alt='img' /></div>
+      <div className="card w-96 bg-indigo-200 text-primary-content">
+        <div className="card-body">
+          <h6 className="text-stone-900">New Arrival</h6>
+          <h2 className="card-title text-indigo-900">Discover Our <br></br> New Collection</h2>
+          <p className="text-stone-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className="card-actions  justify-start">
+          <button className="btn bg-blue-900">Buy Now</button>
+        </div>
+      </div>
+</div>
     </div>
-  </div>
   )
 }
 
